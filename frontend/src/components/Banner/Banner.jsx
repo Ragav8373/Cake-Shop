3960b443197a192cdd9f140334a96d29b7b7461e
Ragav8373 <Ragav9760@gmail.com>
2025-12-08 15:55:25 +0530

    updated:banner

--- frontend/src/components/Banner/Banner.jsx
+++ frontend/src/components/Banner/Banner.jsx
@@ -1,23 +1,42 @@
 import React from "react";
-import bg1 from "../../img2/New folder/logo5.jpg";
-import bg2 from "../../assets/slide4.jpg";
+import bg1 from "../../assets/cake1.jpg";
+import bg2 from "../../assets/cake2.jpg";
+import bg3 from "../../assets/cake3.jpg";
+import bg4 from "../../assets/cake4.jpg";
 import "./Banner.css";
 
 function Banner() {
   return (
     <div className="sec1">
       <div
-        id="carouselExampleSlidesOnly"
-        className="carousel slide"
+        id="carouselExampleFade"
+        className="carousel slide carousel-fade"
         data-bs-ride="carousel"
       >
         <div className="carousel-inner">
+
           <div className="carousel-item active">
             <img src={bg1} className="d-block w-100 bg1" alt="Slide 1" />
           </div>
+
           <div className="carousel-item">
             <img src={bg2} className="d-block w-100 bg1" alt="Slide 2" />
           </div>
+
+          <div className="carousel-item">
+            <img src={bg3} className="d-block w-100 bg1" alt="Slide 3" />
+          </div>
+
+          <div className="carousel-item">
+            <img src={bg4} className="d-block w-100 bg1" alt="Slide 4" />
+          </div>
+
+        </div>
+
+        {/* TEXT OVERLAY */}
+        <div className="banner-text">
+          <h1>Fresh & Delicious Cakes</h1>
+          <p>Made with love. Baked for every celebration.</p>
         </div>
       </div>
     </div>
